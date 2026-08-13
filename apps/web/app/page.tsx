c8ae15f94b3ebbe6813cbd63217987f9d499d2d3
xaxxoo <bnahmad83@gmail.com>
2026-08-13 20:02:01 +0100

    feat(web): add product capability overview

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,4 +1,6 @@
 import { MarketingLayout } from '../components/marketing/marketing-layout';
+import { FragmentationSection } from '../components/marketing/fragmentation-section';
 import { LandingClosing, LandingHero, LandingStory } from '../components/marketing/landing-sections';
+import { ProductOverview } from '../components/marketing/product-overview';
 
-export default function HomePage() { return <MarketingLayout><LandingHero /><LandingStory /><LandingClosing /></MarketingLayout>; }
+export default function HomePage() { return <MarketingLayout><LandingHero /><FragmentationSection /><ProductOverview /><LandingStory /><LandingClosing /></MarketingLayout>; }
