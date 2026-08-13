ec2d235f89d3d3e4115943cecb481d9952bce975
xaxxoo <bnahmad83@gmail.com>
2026-08-13 20:08:54 +0100

    feat(web): add institutional rwa landing section

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,5 +2,6 @@ import { MarketingLayout } from '../components/marketing/marketing-layout';
 import { FragmentationSection } from '../components/marketing/fragmentation-section';
 import { LandingClosing, LandingHero, LandingStory } from '../components/marketing/landing-sections';
 import { ProductOverview } from '../components/marketing/product-overview';
+import { RwaSection } from '../components/marketing/rwa-section';
 
-export default function HomePage() { return <MarketingLayout><LandingHero /><FragmentationSection /><ProductOverview /><LandingStory /><LandingClosing /></MarketingLayout>; }
+export default function HomePage() { return <MarketingLayout><LandingHero /><FragmentationSection /><RwaSection /><ProductOverview /><LandingStory /><LandingClosing /></MarketingLayout>; }
